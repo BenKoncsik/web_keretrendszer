b2e2bc5766cbdc2d6b7cf55c3e0336b9e28d29d2
Koncsik Benedek András <h047161@stud.u-szeged.hu>
2023-04-24 17:14:37 +0200

    add all modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideStorage,getStorage } from '@angular/fire/storage';
+import { AngularFireModule } from '@angular/fire/compat';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularFireModule,
+    //provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,22 @@
+export const environment = {
+  firebase: {
+    projectId: 'chatweb-b566c',
+    appId: '1:337073059438:web:08695bc83aedf8636a587f',
+    storageBucket: 'chatweb-b566c.appspot.com',
+    apiKey: 'AIzaSyDeiUZ205RMZbEONLhekjE061OTt9v7dF8',
+    authDomain: 'chatweb-b566c.firebaseapp.com',
+    messagingSenderId: '337073059438',
+    measurementId: 'G-WVJZ9X0M4Y',
+  },
+    production: false,
+    firebaseConfig: {
+      apiKey: 'YOUR_API_KEY',
+      authDomain: 'YOUR_AUTH_DOMAIN',
+      projectId: 'YOUR_PROJECT_ID',
+      storageBucket: 'YOUR_STORAGE_BUCKET',
+      messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+      appId: 'YOUR_APP_ID',
+      measurementId: 'YOUR_MEASUREMENT_ID',
+    },
+  };
+  
